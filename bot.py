e55aa77ae5dd9122acc1eafcc545d008dcea70f4
Adeboye <sdalfriend@gmail.com>
2026-03-24 15:04:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,27 @@
-import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = os.getenv("8795075971:AAEi-ornLRNPat--RnTOrc1D_NP99hA7Ock")
+# 👉 PASTE YOUR TOKEN HERE
+TOKEN = "8795075971:AAFf2oUcuES7Fu6Vzq6Hx10b53bWr4NSAgo"
 
+
+# /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! Your bot is working 🚀")
+    await update.message.reply_text("Hello 👋 Your bot is working!")
 
+
+# reply to any message
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    await update.message.reply_text(f"You said: {user_message}")
+
+
+# main app
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+print("Bot is running... 🚀")
 
 app.run_polling()
